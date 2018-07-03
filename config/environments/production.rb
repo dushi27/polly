9cc3e23dcc0cff0ce59a5ac59ce8fd034cccba19
dushyanthi pieris <dushi27@gmail.com>
2018-07-02 22:09:52 -0400

    deploy prep

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://pollychart.herokuapp.com/cable' 
+  config.action_cable.allowed_request_origins = [ 'http://pollychart.herokuapp.com', /http:\/\/pollychart.herokuapp*/ ]
+  config.action_cable_redis_url = ENV.fetch('ACTION_CABLE_REDIS_URL', nil)
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
